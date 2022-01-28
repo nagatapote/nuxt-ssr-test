0cbf2c9120ba0383174be5365abaaa027df33de4
nagatapote <hiroaki29p@icloud.com>
2022-01-28 12:30:34 +0900

    fix: axios通信テスト

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    <ul>
+      <li v-for="(post, i) in posts" v-show="i < 3" :key="i">
+        <span>{{ post.url }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ $axios }) {
+    const url = 'https://qiita.com/api/v2/items'
+    const response = await $axios.$get(url)
+    return {
+      posts: response,
+    }
+  },
 }
 </script>
