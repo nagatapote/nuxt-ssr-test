15555de1caccc20da8ec6e1d7a802afd6d3bdae0
nagatapote <hiroaki29p@icloud.com>
2022-01-28 12:36:12 +0900

    fix: logoとtestを再追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <NuxtLogo />
     <ul>
       <li v-for="(post, i) in posts" v-show="i < 3" :key="i">
         <span>{{ post.url }}</span>
@@ -9,8 +10,10 @@
 </template>
 
 <script>
+import NuxtLogo from '~/components/NuxtLogo.vue'
 export default {
   name: 'IndexPage',
+  components: { NuxtLogo },
   async asyncData({ $axios }) {
     const url = 'https://qiita.com/api/v2/items'
     const response = await $axios.$get(url)
