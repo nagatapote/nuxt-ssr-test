e273f30e921821db4d84c87762ef6c6d8ca900ab
nagatapote <hiroaki29p@icloud.com>
2022-02-01 20:17:37 +0900

    add: authorization headers & jwt有効期限切れ時の暫定対応

---
+++ pages/error-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>エラーが発生しました。</p>
+    <button @click="logout">ホームへ戻る</button>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  methods: {
+    ...mapActions(['logout']),
+  },
+}
+</script>
