1a8dc3184fb6364278f399f1fce6119638597e29
nagatapote <hiroaki29p@icloud.com>
2022-02-02 08:32:57 +0900

    add: 404等のエラー時に自動的に遷移するエラー画面を作成

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>ステータスコード：{{ error.statusCode }}</p>
+    <p>メッセージ：{{ error.message }}</p>
+    <NuxtLink to="/">ホームへ戻る</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LayoutsErrorPage',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
