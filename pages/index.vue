26f66f39630cef507774c0e4a0eced6d713de84d
nagatapote <hiroaki29p@icloud.com>
2022-02-05 15:17:10 +0900

    add: 画像データのダウンロードテスト

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
         <span>{{ post }}</span>
       </li>
     </ul>
+    <button @click="fileDownload">ファイルダウンロード</button>
     <button @click="logout">ログアウト</button>
   </div>
 </template>
@@ -29,8 +30,30 @@ export default {
       }
     }
   },
+
   methods: {
     ...mapActions(['logout']),
+    async fileDownload() {
+      const res = await this.$axios.post(
+        '/api/user/download',
+        {
+          filename: 'uploaded_1643203459290932000.jpg',
+        },
+        {
+          responseType: 'blob',
+          dataType: 'binary',
+        }
+      )
+      const blob = new Blob([res.data], { type: 'image/jpeg' })
+      const url = window.URL.createObjectURL(blob)
+
+      const a = document.createElement('a')
+      a.href = url
+      a.download = 'uploaded_1643203459290932000.jpg'
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+    },
   },
 }
 </script>
